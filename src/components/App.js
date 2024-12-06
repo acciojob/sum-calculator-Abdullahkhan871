454734e7fd7f83f6692bf0f315765a9301a89c9e
Abdullahkhan871 <123239280+Abdullahkhan871@users.noreply.github.com>
2024-12-06 21:24:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from 'react'
+import SumCalculator from './SumCalculator'
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+	return (
+		<div>
+			<SumCalculator />
+		</div>
+	)
 }
 
 export default App
